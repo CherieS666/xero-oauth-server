2354b9ae7c6ee775b887266e7b3d5650bd034297
Cherie Sun <Cherie.Sun@aspenpharmacare.com.au>
2026-05-25 20:58:31 +1000

    Update Xero OAuth

--- main.py
+++ main.py
@@ -206,7 +206,7 @@ def callback():
 
     number_of_units = float(row["numberOfUnits"])
 
-    earnings_rate_id = row["EarningsRateID"]
+    earnings_rate_id = row["earningsRateID"]
 
     # =====================================
     # PAYROLL CALENDAR
