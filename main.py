5735d526b20cac1fac5a5418639098c4814356d7
Cherie Sun <Cherie.Sun@aspenpharmacare.com.au>
2026-05-29 20:15:10 +1000

    update excel

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ def home():
             "payroll.employees "
             "payroll.timesheets "
             "payroll.payruns"
-            "payroll.settings"
+
         ),
         "state": state,
     }
